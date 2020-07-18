17eaf2a4bb545ae3ad8e5a3b7b327bbc85c42de3
shadowdolphin1221 <68484019+shadowdolphin1221@users.noreply.github.com>
2020-07-18 18:25:08 -0400

    Create duplicate-emails.sql

---
+++ duplicate-emails.sql
@@ -0,0 +1,23 @@
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+
+SELECT a.Email from
+(
+SELECT Email, COUNT(*) as count from Person 
+group by Email)a
+Where a.count >=2 
